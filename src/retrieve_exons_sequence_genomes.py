38099cb86c394a5f18923ca5a3993d47559768a8
Elfy <elfyly.94@gmail.com>
2020-05-26 15:08:03 +0200

    Update retrieve_exons_sequence_genomes.py

--- src/retrieve_exons_sequence_genomes.py
+++ src/retrieve_exons_sequence_genomes.py
@@ -1,28 +1,30 @@
 # retrieve_exons_sequence_genomes.py
-# This script is to retrieve exons from sequenced genomes
-# To identify the contigs from the sequenced genomes, each contig has to be retrieved from the sequenced genome first.
-# Then, for each sequence query of the sequenced genomes, the query can be BLAT against the database reference.
-# In this case, the database reference will be Arabidopsis thaliana.
+# This script is to retrieve exons from sequenced genomes which are also present in the reference genome (A. thaliana).
+# To identify the contigs from the sequenced genomes, each contig has to be retrieved from A. thaliana first.
+# Then, for each sequence query of A. thaliana, the query can be BLAT against the database reference.
+# In this case, the database reference will be S. irio and A. lyrata.
 # Made by: Elfy Ly
 # Date: 19 May 2020
 
 import os
 from Bio import SeqIO
 
-path_to_sis_database = "/mnt/c/Users/elfyl/PycharmProjects/brassicaceae-hybseq-pipeline-offline/data/sequence_genomes/sis_ref.fna"
-path_to_sis_exons_dir = "/mnt/c/Users/elfyl/PycharmProjects/brassicaceae-hybseq-pipeline-offline/data/sequence_genomes/sis_ref"
+path_to_at_exons_dir = "/mnt/c/Users/elfyl/PycharmProjects/brassicaceae-hybseq-pipeline-offline/results/exons"
+path_to_at_dir = "/mnt/c/Users/elfyl/PycharmProjects/brassicaceae-hybseq-pipeline-offline/data/reference_genomes"
+path_to_at_reference = path_to_at_dir + "/ref-at.fasta"
 
-# Create target Directory if don't exist
-if not os.path.exists(path_to_sis_exons_dir):
-    os.mkdir(path_to_sis_exons_dir)
-    print("Directory ", path_to_sis_exons_dir, " Created ")
+
+# Create exons_AT Directory if don't exist
+if not os.path.exists(path_to_at_exons_dir):
+    os.mkdir(path_to_at_exons_dir)
+    print("Directory ", path_to_at_exons_dir, " Created ")
 else:
-    print("Directory ", path_to_sis_exons_dir, " already exists")
+    print("Directory ", path_to_at_exons_dir, " already exists")
 
-# Create new files for every sequence query of the sis database
+# Create new files for every sequence query of the reference genome A. thaliana
 count_id = 0
-for seq_record in SeqIO.parse(path_to_sis_database, "fasta"):
-    f = open(path_to_sis_exons_dir + "/" + seq_record.id + ".txt", "w+")
+for seq_record in SeqIO.parse(path_to_at_reference, "fasta"):
+    f = open(path_to_at_exons_dir + "/" + seq_record.id + ".txt", "w+")
     print("New text file created: " + seq_record.id + ".fa")
     seq_id = seq_record.id
     seq_seq = str(seq_record.seq)
