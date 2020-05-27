761189e6d030ea10f109cda8af79d7b06e85c528
Elfy <elfyly.94@gmail.com>
2020-05-27 15:34:00 +0200

    update extract_contigs_YASRA.py

--- src/extract_contigs_YASRA.py
+++ src/extract_contigs_YASRA.py
@@ -6,15 +6,15 @@
 
 import os
 
-path_to_YASRA_fSAM = './SRR8528336_reads.fq_ref-at.fasta_Wed-May--6-142648-2020/YASRA_related_files/alignments_SRR8528336_reads.fq_ref-at.fasta.txt'
-path_to_exons = './results/exons/txt'
+path_to_YASRA_fSAM = '/results/alignments/SRR8528336_reads.fq_ref-at.fasta_Mon-May-25-08:50:00-2020/YASRA_related_files/alignments_SRR8528336_reads.fq_ref-at.fasta.sam'
+path_to_assembled_exons = './results/assembled_exons/SRR8628336/txt'
 
 # Create target Directory if don't exist
-if not os.path.exists(path_to_exons):
-    os.mkdir(path_to_exons)
-    print("Directory ", path_to_exons, " Created ")
+if not os.path.exists(path_to_assembled_exons):
+    os.mkdir(path_to_assembled_exons)
+    print("Directory ", path_to_assembled_exons, " Created ")
 else:
-    print("Directory ", path_to_exons, " already exists")
+    print("Directory ", path_to_assembled_exons, " already exists")
 
 ncontigs = 0
 nexons = 0
@@ -30,7 +30,7 @@ with open(path_to_YASRA_fSAM, 'rt') as myfile:
             contig_length = int(contig_end) - int(contig_start) + 1
 
             if contig != contig_name_temporary:
-                f = open(path_to_exons + "/" + contig_number + "_" + reference_genome + ".txt", "w+")
+                f = open(path_to_assembled_exons + "/" + contig_number + "_" + reference_genome + ".txt", "w+")
                 print("New text file created: " + contig_number + "_" + reference_genome)
 
                 f.write("@HD\tVN:1.3\n@SQ\tSN:" + contig + "\tLN:" + str(contig_length) + "\n" + myline)
@@ -40,11 +40,11 @@ with open(path_to_YASRA_fSAM, 'rt') as myfile:
                 contig_name_temporary = contig
 
             elif contig == contig_name_temporary:
-                f = open(path_to_exons + "/" + contig_number + "_" + reference_genome + ".txt", "a+")
+                f = open(path_to_assembled_exons + "/" + contig_number + "_" + reference_genome + ".txt", "a+")
                 f.write(myline)
                 f.close()
 #
-    f = open(path_to_exons + "/number_of_contigs_and_exons.txt", "w+")
+    f = open(path_to_assembled_exons + "/number_of_contigs_and_exons.txt", "w+")
     f.write("Number of contigs: " + str(ncontigs) + "\nNumber of exons: " + str(nexons))
 
 
