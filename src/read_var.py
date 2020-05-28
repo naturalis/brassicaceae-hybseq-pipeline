ec5c1d69e26c4b736643c6431f967f0daa0c62b3
Elfy <elfyly.94@gmail.com>
2020-05-28 12:01:47 +0200

    Update read_var.py

--- src/read_var.py
+++ src/read_var.py
@@ -1,16 +1,23 @@
+#!/usr/bin/python
 # This script is to read lines in var file after the VARscan step
 # It returns the sequence, number of SNPs and avcov
 # Made by: Elfy Ly
 # Date: 28 May 2020
 
+import sys
 import os
 
-SPECIES = "SRR8528336"
+#SPECIES = "SRR8528336"
 CONTIGNR = 8
 
 path_to_assembled_exons_dir = "./results/assembled_exons/"
+
+for root_dir, sub_dir, files in os.walk(path_to_assembled_exons_dir):
+    for species in sub_dir:
+        print(species)
+
 fvar_name = "Contig" + str(CONTIGNR) + "_AT_sort.var"
-path_to_fvar = path_to_assembled_exons_dir + SPECIES + "/var/" + fvar_name
+path_to_fvar = path_to_assembled_exons_dir + species + "/var/" + fvar_name
 
 seq = ""
 nsnps = 0
@@ -40,25 +47,25 @@ print("seq length: " + str(seq_length))
 
 # Create consensus directory if it doesn't exist
 path_to_consensus_dir = "./results/consensus/"
-path_to_consensus_species_dir = path_to_consensus_dir + SPECIES + "/"
+path_to_consensus_species_dir = path_to_consensus_dir + species + "/"
 
 if not os.path.exists(path_to_consensus_dir):
     os.mkdir(path_to_consensus_dir)
-    print("Directory ", path_to_consensus_dir, " Created ")
+    print("Directory " + path_to_consensus_dir + " Created ")
 else:
-    print("Directory ", path_to_consensus_dir, " already exists")
+    print("Directory " + path_to_consensus_dir + " already exists")
 
 if not os.path.exists(path_to_consensus_species_dir):
     os.mkdir(path_to_consensus_species_dir)
-    print("Directory ", path_to_consensus_species_dir, " Created ")
+    print("Directory " + path_to_consensus_species_dir + " Created ")
 else:
-    print("Directory ", path_to_consensus_species_dir, " already exists")
+    print("Directory " + path_to_consensus_species_dir + " already exists")
 
 
 ncontigs = 0
 if seq_length > 0:
-    f = open(path_to_consensus_species_dir + "Contig " + str(CONTIGNR) + ".txt", "w+")
-    print("New text file created: " + "Contig " + str(CONTIGNR) + ".fa")
+    f = open(path_to_consensus_species_dir + "Contig" + str(CONTIGNR) + ".txt", "w+")
+    print("New text file created: " + "Contig" + str(CONTIGNR) + ".txt")
     f.write(">Contig" + str(CONTIGNR) + "\n" + seq + "\n")
     f.close()
     ncontigs += 1
