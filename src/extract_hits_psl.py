ca46805dc86a44a6c47f4ee2d21ffff958084243
Elfy <elfyly.94@gmail.com>
2020-06-03 15:28:06 +0200

    Update extract_hits_psl.py
    finalizing script

--- src/extract_hits_psl.py
+++ src/extract_hits_psl.py
@@ -9,17 +9,15 @@ from Bio import SearchIO
 
 def create_fout(path_to_fout):
     f = open(path_to_fout, "w+")
-    f.write("tName\tqName\tpercent ID\tPSL score\n")
+    f.write("target_name\tquery_name\tpercent_ID\tPSL_score\n")
     f.close()
+
 
 # reads psl file and returns the t_name, q_name, id_pct and score for highest hit per contig
 def read_psl(path_to_psl):
     qresult = SearchIO.read(path_to_psl, "blat-psl")
     n_hits = len(qresult)
-    # print("number of hits: " + str(n_hits))
 
-    # max_hsp_hit = 0
-    # max_hit = 0
     t_name_highest = ""
     q_name_highest = ""
     id_pct_highest = 0
@@ -28,28 +26,11 @@ def read_psl(path_to_psl):
         n_hsp_hits = len(qresult[hit])
         for n in range(n_hsp_hits):  # loops over hsp
             blat_hsp = qresult[hit][n]
-            hit_number = n + 1
-            # print("hit: " + str(n+1))
-            # print("hit_id: " + qresult.id)
-            # print("hsp_hit_id: " + blat_hsp.hit_id)
-            # print("score: " + str(blat_hsp.score))
-            # print("percent ID: " + str(blat_hsp.ident_pct) + "\n")
-
             if blat_hsp.score > max_score:
                 t_name_highest = qresult.id
                 q_name_highest = blat_hsp.hit_id
                 id_pct_highest = blat_hsp.ident_pct
                 max_score = blat_hsp.score
-                # max_hsp_hit = hit_number
-                # max_hit = hit
-
-    # prints out highest scores information for every contig
-    # print("tname: " + t_name_highest)
-    # print("best_exon: " + q_name_highest)
-    # print("highest hsp pct ID: " + str(id_pct_highest))
-    # print("max_hit_nr: " + str(max_hit))
-    # print("max_hsp_hit: " + str(max_hsp_hit))
-    # print("max_hsp_score: " + str(max_score) + "\n")
 
     # highest BLAT hits are written per contig per species
     write_fout(path_to_fout, t_name_highest, q_name_highest, id_pct_highest, max_score)
