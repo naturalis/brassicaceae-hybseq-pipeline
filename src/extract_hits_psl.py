6556ce770b0613a6f93abbf06619e8f81b60bc6f
Elfy <elfyly.94@gmail.com>
2020-06-03 12:31:15 +0200

    Update extract_hits_psl.py

--- src/extract_hits_psl.py
+++ src/extract_hits_psl.py
@@ -5,13 +5,6 @@
 
 from Bio import SearchIO
 
-# def calculate_sizeMul(blat_hsp):
-#     if blat_hsp.query_is_protein:
-#         sizeMul = 3
-#     else:
-#         sizeMul = 1
-#     return sizeMul
-
 # moet loopen over contig nummers
 path_to_psl = "./results/blat/SRR8528336/contig1_AT.psl"
 path_to_fout = "./results/blat/SRR8528336/highest_hits.txt"
@@ -28,18 +21,16 @@ t_name_highest = ""
 q_name_highest = ""
 id_pct_highest = 0
 max_score = 0
+max_hsp_hit = 0
 max_hit = 0
 for hit in range(n_hits):           # loops over hits
     for n in range(n_hsp_hits):      # loops over hsp
         blat_hsp = qresult[hit][n]
         hit_number = n+1
         # print("hit: " + str(n+1))
-        # print(blat_hsp.hit_id)
-
-        # calculate_sizeMul(blat_hsp)
-
-        # print("sizeMul: " + str(sizeMul))
-        # print("score: " + str(blat_hsp.score))
+        print("hit_id: " + qresult.id)
+        print("hsp_hit_id: " + blat_hsp.hit_id)
+        print("score: " + str(blat_hsp.score))
         # print("percent ID: " + str(blat_hsp.ident_pct) + "\n")
 
         if blat_hsp.score > max_score:
@@ -47,13 +38,15 @@ for hit in range(n_hits):           # loops over hits
             q_name_highest = blat_hsp.hit_id
             id_pct_highest = blat_hsp.ident_pct
             max_score = blat_hsp.score
-            max_hit = hit_number
+            max_hsp_hit = hit_number
+            max_hit = hit
 
-    print("tname: " + t_name_highest)
-    print("best_exon: " + q_name_highest)
-    print("highest hsp pct ID: " + str(id_pct_highest))
-    print("max_hsp_hit: " + str(max_hit))
-    print("max_hsp_score: " + str(max_score))
+print("tname: " + t_name_highest)
+print("best_exon: " + q_name_highest)
+print("highest hsp pct ID: " + str(id_pct_highest))
+print("max_hit_nr: " + str(max_hit))
+print("max_hsp_hit: " + str(max_hsp_hit))
+print("max_hsp_score: " + str(max_score))
 
 f = open(path_to_fout, "w+")
 f.write("tName\tqName\tpercent ID\tPSL score\n")
