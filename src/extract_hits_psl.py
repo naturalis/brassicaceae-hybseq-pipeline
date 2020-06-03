62b49c2ae69ffff333dcf6643f794e9e076dd64d
Elfy <elfyly.94@gmail.com>
2020-06-03 12:21:28 +0200

    Update extract_hits_psl.py

--- src/extract_hits_psl.py
+++ src/extract_hits_psl.py
@@ -5,14 +5,18 @@
 
 from Bio import SearchIO
 
-MIN_HITS = 5
+# def calculate_sizeMul(blat_hsp):
+#     if blat_hsp.query_is_protein:
+#         sizeMul = 3
+#     else:
+#         sizeMul = 1
+#     return sizeMul
 
-path_to_psl = "/mnt/c/Users/elfyl/PycharmProjects/brassicaceae-hybseq-pipeline-offline/results/blat/at_sis/at_sis_KE154134.1.psl"          # 45 matches
-path_to_psl_2 = "/mnt/c/Users/elfyl/PycharmProjects/brassicaceae-hybseq-pipeline-offline/results/blat/at_sis/at_sis_ASZH01000019.1.psl"    # should be empty
-path_to_psl_3 = "/mnt/c/Users/elfyl/PycharmProjects/brassicaceae-hybseq-pipeline-offline/results/blat/at_sis/at_sis_KE154096.1.psl"        # 29 matches
-path_to_psl_4 = "/results/blat/SRR8528336/contig1_AT.psl"
+# moet loopen over contig nummers
+path_to_psl = "./results/blat/SRR8528336/contig1_AT.psl"
+path_to_fout = "./results/blat/SRR8528336/highest_hits.txt"
 
-qresult = SearchIO.read(path_to_psl_3, "blat-psl")
+qresult = SearchIO.read(path_to_psl, "blat-psl")
 # print(qresult)
 # print("length: " + str(len(qresult[0])) + "\n")
 print(qresult)
@@ -20,32 +24,44 @@ n_hits = len(qresult)
 print(n_hits)
 n_hsp_hits = len(qresult[0])
 
-highest_id_pct = 0
+t_name_highest = ""
+q_name_highest = ""
+id_pct_highest = 0
 max_score = 0
 max_hit = 0
-best_exon = ""
-for n in range(n_hsp_hits):
-    blat_hsp = qresult[0][n]      # first hit, loop over hsp
-    hit_number = n+1
-    # print("hit: " + str(n+1))
-    # print(blat_hsp.hit_id)
-    if blat_hsp.query_is_protein:
-        sizeMul = 3
-    else:
-        sizeMul = 1
-    # print("sizeMul: " + str(sizeMul))
-    # print("score: " + str(blat_hsp.score))
-    # print("percent ID: " + str(blat_hsp.ident_pct) + "\n")
-    if blat_hsp.score > max_score:
-        highest_id_pct = blat_hsp.ident_pct
-        max_score = blat_hsp.score
-        max_hit = hit_number
-        best_exon = blat_hsp.hit_id
+for hit in range(n_hits):           # loops over hits
+    for n in range(n_hsp_hits):      # loops over hsp
+        blat_hsp = qresult[hit][n]
+        hit_number = n+1
+        # print("hit: " + str(n+1))
+        # print(blat_hsp.hit_id)
 
-print("max_score: " + str(max_score))
-print("max_hit: " + str(max_hit))
-print("highest pct ID: " + str(highest_id_pct))
-print("best_exon: " + best_exon)
+        # calculate_sizeMul(blat_hsp)
+
+        # print("sizeMul: " + str(sizeMul))
+        # print("score: " + str(blat_hsp.score))
+        # print("percent ID: " + str(blat_hsp.ident_pct) + "\n")
+
+        if blat_hsp.score > max_score:
+            t_name_highest = qresult.id
+            q_name_highest = blat_hsp.hit_id
+            id_pct_highest = blat_hsp.ident_pct
+            max_score = blat_hsp.score
+            max_hit = hit_number
+
+    print("tname: " + t_name_highest)
+    print("best_exon: " + q_name_highest)
+    print("highest hsp pct ID: " + str(id_pct_highest))
+    print("max_hsp_hit: " + str(max_hit))
+    print("max_hsp_score: " + str(max_score))
+
+f = open(path_to_fout, "w+")
+f.write("tName\tqName\tpercent ID\tPSL score\n")
+f.write(t_name_highest + "\t" + q_name_highest + "\t" + str(id_pct_highest) + "\t" + str(max_hit) + "\t" +
+        str(max_score))
+f.close()
+
+
 
 
 
