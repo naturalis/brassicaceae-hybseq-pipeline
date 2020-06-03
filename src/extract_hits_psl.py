34b256a68b1e00231000d8dbdd188717bb9a3a39
Elfy <elfyly.94@gmail.com>
2020-06-03 17:19:10 +0200

    Update extract_hits_psl.py
    add function to check empty file

--- src/extract_hits_psl.py
+++ src/extract_hits_psl.py
@@ -13,38 +13,41 @@ def create_fout(path_to_fout):
     f.close()
 
 
-# reads psl file and returns the t_name, q_name, id_pct and score for highest hit per contig
+# reads psl file
 def read_psl(path_to_psl):
-    # checks if empty
-    nlines = 0
+    # checks if psl file is empty
     with open(path_to_psl, 'rt') as myfile:
         for myline in myfile:
-            nlines += 1
-        if nlines > 1:
-            qresult = SearchIO.read(path_to_psl, "blat-psl")
-            n_hits = len(qresult)
-
-            t_name_highest = ""
-            q_name_highest = ""
-            id_pct_highest = 0
-            max_score = 0
-            for hit in range(n_hits):  # loops over hits
-                n_hsp_hits = len(qresult[hit])
-                for n in range(n_hsp_hits):  # loops over hsp
-                    blat_hsp = qresult[hit][n]
-                    if blat_hsp.score > max_score:
-                        t_name_highest = qresult.id
-                        q_name_highest = blat_hsp.hit_id
-                        id_pct_highest = blat_hsp.ident_pct
-                        max_score = blat_hsp.score
-
-            # highest BLAT hits are written per contig per species
-            write_fout(path_to_fout, t_name_highest, q_name_highest, id_pct_highest, max_score)
-        else:
-            print(path_to_psl + " is empty")
+            if len(myline[1].strip()) == 0:
+                print(path_to_psl + " is empty")
+            else:
+                t_name_highest, q_name_highest, id_pct_highest, max_score = extract_hits(path_to_psl)
+                write_fout(path_to_fout, t_name_highest, q_name_highest, id_pct_highest, max_score)
 
 
-# add highest scored exons per contig in highest_hits.txt file
+# extract hits and returns the t_name, q_name, id_pct and score for highest hit from psl file per contig per species
+def extract_hits(path_to_psl):
+    qresult = SearchIO.read(path_to_psl, "blat-psl")
+    n_hits = len(qresult)
+
+    t_name_highest = ""
+    q_name_highest = ""
+    id_pct_highest = 0
+    max_score = 0
+    for hit in range(n_hits):  # loops over hits
+        n_hsp_hits = len(qresult[hit])
+        for n in range(n_hsp_hits):  # loops over hsp
+            blat_hsp = qresult[hit][n]
+            if blat_hsp.score > max_score:
+                t_name_highest = qresult.id
+                q_name_highest = blat_hsp.hit_id
+                id_pct_highest = blat_hsp.ident_pct
+                max_score = blat_hsp.score
+
+    return t_name_highest, q_name_highest, id_pct_highest, max_score
+
+
+# add highest BLAT scored exons hits per contig per species in highest_hits.txt file
 def write_fout(path_to_fout, t_name_highest, q_name_highest, id_pct_highest, max_score):
     f = open(path_to_fout, "a+")
     f.write(t_name_highest + "\t" + q_name_highest + "\t" + str(id_pct_highest) + "\t" + str(max_score) + "\n")
