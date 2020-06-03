e00d3ecd5a7462889bbf6e631566455a36abfdb3
Elfy <elfyly.94@gmail.com>
2020-06-03 13:43:00 +0200

    Update extract_hits_psl.py
    added functions and loops over contigs in directory

--- src/extract_hits_psl.py
+++ src/extract_hits_psl.py
@@ -3,58 +3,79 @@
 # Made by: Elfy Ly
 # Date: 22 May 2020
 
+import os, fnmatch
 from Bio import SearchIO
 
-# moet loopen over contig nummers
-path_to_psl = "./results/blat/SRR8528336/contig1_AT.psl"
-path_to_fout = "./results/blat/SRR8528336/highest_hits.txt"
 
-qresult = SearchIO.read(path_to_psl, "blat-psl")
-# print(qresult)
-# print("length: " + str(len(qresult[0])) + "\n")
-print(qresult)
-n_hits = len(qresult)
-print(n_hits)
-n_hsp_hits = len(qresult[0])
+# reads psl file and returns the t_name, q_name, id_pct and score for highest hit per contig
+def read_psl(path_to_psl, t_name_highest, q_name_highest, id_pct_highest, max_score):
+    qresult = SearchIO.read(path_to_psl, "blat-psl")
+    n_hits = len(qresult)
+    print("number of hits: " + str(n_hits))
+    n_hsp_hits = len(qresult[0])
+
+    max_hsp_hit = 0
+    max_hit = 0
+
+    for hit in range(n_hits):  # loops over hits
+        for n in range(n_hsp_hits):  # loops over hsp
+            blat_hsp = qresult[hit][n]
+            hit_number = n + 1
+            # print("hit: " + str(n+1))
+            print("hit_id: " + qresult.id)
+            print("hsp_hit_id: " + blat_hsp.hit_id)
+            print("score: " + str(blat_hsp.score))
+            # print("percent ID: " + str(blat_hsp.ident_pct) + "\n")
+
+            if blat_hsp.score > max_score:
+                t_name_highest = qresult.id
+                q_name_highest = blat_hsp.hit_id
+                id_pct_highest = blat_hsp.ident_pct
+                max_score = blat_hsp.score
+                max_hsp_hit = hit_number
+                max_hit = hit
+
+    # prints out highest scores information for every contig
+    print("tname: " + t_name_highest)
+    print("best_exon: " + q_name_highest)
+    print("highest hsp pct ID: " + str(id_pct_highest))
+    print("max_hit_nr: " + str(max_hit))
+    print("max_hsp_hit: " + str(max_hsp_hit))
+    print("max_hsp_score: " + str(max_score) + "\n")
+
+    return t_name_highest, q_name_highest, id_pct_highest, max_score
+
+
+# writes highest scored exons per contig in highest_hits.txt file
+def write_fout(path_to_fout, t_name_highest, q_name_highest, id_pct_highest, max_score):
+    f = open(path_to_fout, "w+")
+    f.write("tName\tqName\tpercent ID\tPSL score\n")
+    f.write(t_name_highest + "\t" + q_name_highest + "\t" + str(id_pct_highest) + "\t" + str(max_score) + "\n")
+    f.close()
+
+
+# Code starts here:
+# moet nog loopen over species
+path_to_psl_dir = "./results/blat/SRR8528336/"
+list_in_psl_dir = os.listdir(path_to_psl_dir)
+pattern = "*.psl"
 
 t_name_highest = ""
 q_name_highest = ""
 id_pct_highest = 0
 max_score = 0
-max_hsp_hit = 0
-max_hit = 0
-for hit in range(n_hits):           # loops over hits
-    for n in range(n_hsp_hits):      # loops over hsp
-        blat_hsp = qresult[hit][n]
-        hit_number = n+1
-        # print("hit: " + str(n+1))
-        print("hit_id: " + qresult.id)
-        print("hsp_hit_id: " + blat_hsp.hit_id)
-        print("score: " + str(blat_hsp.score))
-        # print("percent ID: " + str(blat_hsp.ident_pct) + "\n")
 
-        if blat_hsp.score > max_score:
-            t_name_highest = qresult.id
-            q_name_highest = blat_hsp.hit_id
-            id_pct_highest = blat_hsp.ident_pct
-            max_score = blat_hsp.score
-            max_hsp_hit = hit_number
-            max_hit = hit
+# loops over psl files and read them one by one
+# parse the highest target name, query name, ID percentage and score
+# paste them in a file called highest_hits.txt for every species
+for entry in list_in_psl_dir:
+    if fnmatch.fnmatch(entry, pattern):
+        psl_file = entry
+        path_to_psl = path_to_psl_dir + psl_file
 
-print("tname: " + t_name_highest)
-print("best_exon: " + q_name_highest)
-print("highest hsp pct ID: " + str(id_pct_highest))
-print("max_hit_nr: " + str(max_hit))
-print("max_hsp_hit: " + str(max_hsp_hit))
-print("max_hsp_score: " + str(max_score))
+        t_name_highest, q_name_highest, id_pct_highest, max_score = \
+            read_psl(path_to_psl, t_name_highest, q_name_highest, id_pct_highest, max_score)
 
-f = open(path_to_fout, "w+")
-f.write("tName\tqName\tpercent ID\tPSL score\n")
-f.write(t_name_highest + "\t" + q_name_highest + "\t" + str(id_pct_highest) + "\t" + str(max_hit) + "\t" +
-        str(max_score))
-f.close()
-
-
-
-
-
+        # creates a highest_hits.txt file where all highest BLAT hits are written per contig per species
+        path_to_fout = "./results/blat/SRR8528336/highest_hits.txt"
+        write_fout(path_to_fout, t_name_highest, q_name_highest, id_pct_highest, max_score)
