5fea52f0d2f35908db30ec587983e8cb9481f73d
Elfy <elfyly.94@gmail.com>
2020-06-05 10:42:14 +0200

    Update extract_hits_psl.py
    add function to check overlap

--- src/extract_hits_psl.py
+++ src/extract_hits_psl.py
@@ -25,6 +25,25 @@ def create_dictionary_start_end(path_to_list, dictionary):
         for line in f:
             (name, start, end) = line.split()
             dictionary[name] = start, end
+    f.close()
+    return dictionary
+
+
+def check_overlap(sorted_dictionary_contigs, sorted_dictionary_exons):
+    for contig in sorted_dictionary_contigs:
+        contig_start = dictionary_contigs[contig][0]
+        contig_end = dictionary_contigs[contig][1]
+
+        for exon in sorted_dictionary_exons:
+            exon_start = dictionary_exons[exon][0]
+            exon_end = dictionary_exons[exon][1]
+
+            # checks if overlap, if yes then writes contig exon pair in contig_exon_match_list.txt
+            if exon_start <= contig_start <= exon_end or exon_start <= contig_end <= exon_end or \
+                    contig_start <= exon_start <= contig_end or contig_start <= exon_end <= contig_end:
+                f = open(path_to_contig_exon_match, "a+")
+                f.write(contig + ' ' + exon + '\n')
+                f.close()
 
 
 # checks if psl file is empty, if no: reads psl file
@@ -69,29 +88,40 @@ def write_fout(path_to_fout, t_name_highest, q_name_highest, id_pct_highest, max
     f.close()
 
 
-# Code starts here:
+# # # Code starts here:
 # creates a new empty highest_hits.txt file
 path_to_fout = "./results/blat/SRR8528336/highest_hits.txt"
 create_fout(path_to_fout)
 
+# creates separate dictionary for mapped contigs with their start and end position
+path_to_mapped_contigs = './results/assembled_exons/SRR8528336/mapped_contigs.txt'
+dictionary_contigs = {}
+dictionary_contigs = create_dictionary_start_end(path_to_mapped_contigs, dictionary_contigs)
+sorted_dictionary_contigs = natural_sort(dictionary_contigs)
+
+# creates separate dictionary for target exons with their start and end position
+path_to_exons_enum = './data/exons/AT_exon_enum.txt'
+dictionary_exons = {}
+dictionary_exons = create_dictionary_start_end(path_to_exons_enum, dictionary_exons)
+sorted_dictionary_exons = natural_sort(dictionary_exons)
+
+# create an empty contig_exon_match_list.txt
+path_to_contig_exon_match = "./results/assembled_exons/SRR8528336/contig_exon_match_list.txt"
+f = open(path_to_contig_exon_match, "w+")
+f.close()
+
+# checks which mapped contig starting and ending position matches with exons starting and ending position
+# and puts the matching pairs in contig_exon_match_list.txt
+check_overlap(sorted_dictionary_contigs, sorted_dictionary_exons)
+
+# loops over psl files in the psl dir
 # moet nog loopen over species
 path_to_psl_dir = "./results/blat/SRR8528336/"
 list_in_psl_dir = os.listdir(path_to_psl_dir)
 list_in_psl_dir_sorted = natural_sort(list_in_psl_dir)
 pattern = "*.psl"
 
-# creates separate dictionary for mapped contigs and target exons with their start and end position
-path_to_mapped_contigs = './results/assembled_exons/SRR8528336/mapped_contigs.txt'
-path_to_exons_enum = './data/exons/AT_exon_enum.txt'
-dictionary_contigs = {}
-dictionary_exons = {}
-create_dictionary_start_end(path_to_mapped_contigs, dictionary_contigs)
-create_dictionary_start_end(path_to_exons_enum, dictionary_exons)
-
-print(dictionary_exons['>AT1G01220.1@1'])
-print(dictionary_contigs['>Contig1'])
-
-# loops over psl files and read them one by one
+# read the psl files one by one
 # parse the highest target name, query name, ID percentage and score
 # paste them in a file called highest_hits.txt for every species
 for entry in list_in_psl_dir_sorted:
@@ -99,3 +129,5 @@ for entry in list_in_psl_dir_sorted:
         psl_file = entry
         path_to_psl = path_to_psl_dir + psl_file
         read_psl(path_to_psl)
+
+
