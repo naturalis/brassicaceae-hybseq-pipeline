41741a5227654f64038c22dfeb30e476287700ea
Elfy <elfyly.94@gmail.com>
2020-07-31 16:57:45 +0200

    Update files

--- src/read_var.py
+++ src/read_var.py
@@ -9,39 +9,7 @@
 import sys
 import os
 
-SAMPLE_NAME = sys.argv[1]
-
-
-def count_contigs(path_to_var_dir):
-    n_contig_files = 0
-    for contig_file in os.listdir(path_to_var_dir):
-        n_contig_files += 1
-    return n_contig_files
-
-
-# parse var file information for every line and calculates and stores seq, nSNPs and avcov
-def calculate_var_stats(path_to_fvar, seq):
-    nsnps = 0
-    avcov = 0
-    with open(path_to_fvar, 'rt') as myfile:
-        for myline in myfile:
-            if not myline.startswith('Chrom'):
-                chrom, position, ref, cons, reads1, reads2, varfreq, strands1, strands2, qual1, qual2, pvalue, \
-                mapqual1, mapqual2, reads1plus, reads1minus, reads2plus, reads2minus, varallele = myline.split("\t")
-
-                if cons != 'N':
-                    seq += cons
-                elif cons == 'N':
-                    nsnps += 1
-                    avcov += int(reads2)
-
-        if nsnps > 0:
-            avcov /= nsnps
-
-        # print("avcov: " + str(avcov))
-        # print("seq: " + seq)
-        # print("nsnps: " + str(nsnps))
-        return seq
+INPUT_FILE = sys.argv[1]
 
 
 def create_dir(path):
@@ -54,77 +22,103 @@ def create_dir(path):
 
 # Creates new file for whole consensus.txt
 def create_fconsensus(path):
-    f = open(path + "consensus.txt", "w+")
-    print("New text file created :" + path + "consensus.txt")
-    f.close()
-
-
-# Creates new txt file for consensus sequence
-def make_consensus(seq_length, path, contig_number, seq, ncontigs_after_VARscan):
-    if seq_length > 0:
-        append_fconsensus(path, contig_number, seq)
-        create_fcontig_consensus(path, contig_number, seq)
-        ncontigs_after_VARscan += 1
+    fconsensus = path + "consensus.txt"
+    if not os.path.exists(fconsensus):
+        f = open(fconsensus, "w+")
+        print("New text file created :" + fconsensus)
+        f.close()
     else:
-        append_empty_contigs(path_to_consensus_species_dir, contig_number)
-        create_fcontig_consensus(path, contig_number, seq)
-        print("Fasta file is empty: " + "Contig" + str(contig_number) + ".txt")
-
-    return ncontigs_after_VARscan
-
-
-def append_fconsensus(path, contig_number, seq):
-    f = open(path + "consensus.txt", "a+")
-    f.write(">Contig" + str(contig_number) + "\n" + seq + "\n")
-    f.close()
-
-
-def create_fcontig_consensus(path_to_consensus_species_dir, contig_number, seq):
-    f = open(path_to_consensus_species_dir + "Contig" + str(contig_number) + ".fasta", "w+")
-    # print("New text file created: " + "Contig" + str(contig_number) + ".txt")
-    f.write(">Contig" + str(contig_number) + "\n" + seq + "\n")
-    f.close()
+        print(fconsensus + " already exists")
 
 
 def create_file_empty(path_to_consensus_species_dir):
-    f = open(path_to_consensus_species_dir + "empty_contigs.txt", "w+")
+    fempty = path_to_consensus_species_dir + "empty_contigs.txt"
+    if not os.path.exists(fempty):
+        f = open(fempty, "w+")
+        f.close()
+        print("New text file created: empty_contigs.txt")
+    else:
+        print("Directory " + fempty + " already exists")
+
+
+# parse var file information for every line and calculates and stores seq, nSNPs and avcov
+def calculate_var_stats(seq):
+    nsnps = 0
+    # avcov = 0
+    with open(INPUT_FILE, 'rt') as myfile:
+        for myline in myfile:
+            if not myline.startswith('Chrom'):
+                chrom, position, ref, cons, reads1, reads2, varfreq, strands1, strands2, qual1, qual2, pvalue, \
+                mapqual1, mapqual2, reads1plus, reads1minus, reads2plus, reads2minus, varallele = myline.split("\t")
+
+                if cons != 'N':
+                    seq += cons
+                elif cons == 'N':
+                    nsnps += 1
+
+        return seq
+
+
+# Creates new txt file for consensus sequence
+def make_consensus(seq_length, path, contig_nr, seq):
+    if seq_length > 0:
+        append_fconsensus(path, contig_nr, seq)
+        create_fcontig_consensus(path, contig_nr, seq)
+    else:
+        append_empty_contigs(path_to_consensus_species_dir, contig_nr)
+        create_fcontig_consensus(path, contig_nr, seq)
+        print("Fasta file is empty: " + "Contig" + contig_nr + ".txt")
+
+
+def append_fconsensus(path, contig_nr, seq):
+    path_to_fconsensus = path + "consensus.txt"
+    if os.stat(path_to_fconsensus).st_size == 0:
+        f = open(path_to_fconsensus, "a+")
+        f.write(">Contig" + contig_nr + "\n" + seq + "\n")
+        f.close()
+    else:
+        contig_check = ">Contig" + contig_nr
+        if not string_in_file(path_to_fconsensus, contig_check):
+            f = open(path_to_fconsensus, "a+")
+            f.write(">Contig" + contig_nr + "\n" + seq + "\n")
+            f.close()
+
+
+def string_in_file(file_name, string_to_search):
+    """ Check if any line in the file contains given string """
+    with open(file_name, 'r') as read_obj:
+        for line in read_obj:
+            if string_to_search in line:
+                return True
+    return False
+
+
+def create_fcontig_consensus(path, contig_nr, seq):
+    f = open(path + "Contig" + contig_nr + ".fasta", "w+")
+    f.write(">Contig" + contig_nr + "\n" + seq + "\n")
     f.close()
-    print("New text file created: empty_contigs.txt")
 
 
-def append_empty_contigs(path_to_consensus_species_dir, contig_number):
+def append_empty_contigs(path_to_consensus_species_dir, contig_nr):
     f = open(path_to_consensus_species_dir + "empty_contigs.txt", "a+")
-    f.write(">Contig" + str(contig_number) + "\n")
+    f.write(">Contig" + contig_nr + "\n")
     f.close()
 
 
 # Code starts here
-path_to_mapped_contigs_dir = "./results/A04_mapped_contigs/"
-path_to_var_dir = path_to_mapped_contigs_dir + SAMPLE_NAME + "/var/"
-n_contig_files = count_contigs(path_to_var_dir)
+results_dir, A_dir, sample, var_dir, fvar = INPUT_FILE.split("/")
+contig, at, sort = fvar.split("_")
+contig, contig_nr = contig.split("Contig")
 
-# Create consensus directory en file for every species if it doesn't exist
 path_to_consensus_dir = "./results/A05_consensus_contigs/"
-path_to_consensus_species_dir = path_to_consensus_dir + SAMPLE_NAME + "/"
-
 create_dir(path_to_consensus_dir)
+path_to_consensus_species_dir = path_to_consensus_dir + sample + "/"
 create_dir(path_to_consensus_species_dir)
 create_fconsensus(path_to_consensus_species_dir)
 create_file_empty(path_to_consensus_species_dir)
 
-# Loops through all contig files for each species
-ncontigs_after_VARscan = 0
-for contig_number in range(1, n_contig_files + 1):
-    fvar_name = "Contig" + str(contig_number) + "_AT_sort.var"
-    path_to_fvar = path_to_var_dir + fvar_name
+seq = ""
+seq = calculate_var_stats(seq)
+seq_length = len(seq)
+make_consensus(seq_length, path_to_consensus_species_dir, contig_nr, seq)
 
-    seq = ""
-    seq = calculate_var_stats(path_to_fvar, seq)
-    seq_length = len(seq)
-    # print("seq length: " + str(seq_length))
-
-    ncontigs_after_VARscan = make_consensus(seq_length, path_to_consensus_species_dir, contig_number, seq,
-                                            ncontigs_after_VARscan)
-
-# Prints number of contig files created after VARscan (some files are empty after VARscan)
-print("contigs after VARscan: " + str(ncontigs_after_VARscan))
